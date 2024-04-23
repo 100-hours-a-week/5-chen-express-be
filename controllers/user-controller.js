79e54e09197a011e6b0417456efc818cbd1c25d7
ZZ3n <ndsrhkd@naver.com>
2024-04-23 11:17:13 +0900

    refac user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,19 +1,23 @@
 const HttpStatus = require("http-status-codes");
 const {body, param} = require("express-validator");
 const UserModel = require("../models/user-model");
+const fs = require("fs");
+const e = require("express");
 
 
 module.exports = new class {
     exist = {
-        validator: [body("email").trim(), body("nickname").trim()],
+        validator: [
+            body("email").trim(),
+            body("nickname").trim()
+        ],
         controller: (req, res) => {
             const {nickname, email} = req.body
-            const userAll = UserModel.all();
 
             let emailCheck = false;
             let nicknameCheck = false;
 
-            for (const user of userAll) {
+            for (const user of UserModel.all()) {
                 if (email !== undefined) {
                     emailCheck = emailCheck || (email === user.email);
                 }
@@ -22,12 +26,71 @@ module.exports = new class {
                 }
             }
 
-
             res.json({
                 "msg": "OK",
                 "email-exist": emailCheck,
                 "nickname-exist": nicknameCheck,
             })
+        }
+    }
+
+    login = {
+        validator: [
+            body("email").trim(),
+            body("password").trim()
+        ],
+        controller: (req, res) => {
+            const {email, password} = req.body;
+
+            for (user of UserModel.all()) {
+                if (user.email === email && user.password === password) {
+                    res.json({
+                        "msg": "login success"
+                    });
+                    return;
+                }
+            }
+
+            res.status(HttpStatus.UNAUTHORIZED);
+            res.json({
+                "msg": "login failed"
+            });
+        }
+    }
+
+    signup = {
+        validator: [
+            body('email').trim(),
+            body('password').trim(),
+            body('nickname').trim()
+        ],
+        controller: (req, res) => {
+            const {email, password, nickname} = req.body;
+
+            const terminateByDuplicated = resp => {
+                resp.status(HttpStatus.BAD_REQUEST);
+                resp.json({
+                    "msg": "DUPLICATED"
+                });
+            };
+
+            for (const user of UserModel.all()) {
+                if (email === user.email) {
+                    terminateByDuplicated(res);
+                    return;
+                }
+                if (nickname === user.nickname) {
+                    terminateByDuplicated(res);
+                    return;
+                }
+            }
+
+            const newUser = UserModel.create(email, password, nickname);
+            newUser.save()
+
+            res.json({
+                "msg": "OK"
+            });
         }
     }
 }
--- routes/index-route.js
+++ routes/index-route.js
@@ -2,79 +2,12 @@ var express = require('express');
 const HttpStatus = require("http-status-codes")
 const {body} = require("express-validator");
 var router = express.Router();
+const users = require("../controllers/user-controller")
 const fs = require('fs');
 
 /* GET home page. */
-router.post('/login', [
-        body("email").trim(),
-        body("password").trim()
-    ],
-    (req, res) => {
-        const usersData = jsonParse("users.json");
-        for (user of usersData.users) {
-            if (user.email === req.body.email && user.password === req.body.password) {
-                res.json({
-                    "msg": "login success"
-                });
-                return;
-            }
-        }
+router.post('/login', users.login.validator, users.login.controller);
 
-        res.status(HttpStatus.UNAUTHORIZED);
-        res.json({
-            "msg": "login failed"
-        });
-    });
-
-router.post('/signup', [
-        body('email').trim(),
-        body('password').trim(),
-        body('nickname').trim()
-    ], (req, res) => {
-        const userData = jsonParse("users.json");
-        const users = userData.users;
-
-        const {email, password, nickname} = req.body;
-
-        const terminateByDuplicated = resp => {
-            resp.status(HttpStatus.BAD_REQUEST);
-            resp.json({
-                "msg": "DUPLICATED"
-            });
-        };
-
-        let nextId = 0;
-        for (const user of users) {
-            if (email === user.email) {
-                terminateByDuplicated(res);
-                return;
-            }
-            if (nickname === user.nickname) {
-                terminateByDuplicated(res);
-                return;
-            }
-            if (user.id > nextId) {
-                nextId = user.id;
-            }
-        }
-
-        userData.users.push({
-            "id": nextId,
-            "email": email,
-            "password": password,
-            "nickname": nickname
-        });
-
-        fs.writeFileSync(
-            jsonPath("users.json"),
-            JSON.stringify(userData),
-            'utf8'
-        );
-
-        res.json({
-            "msg": "OK"
-        });
-    }
-)
+router.post('/signup', users.signup.validator, users.signup.controller)
 
 module.exports = router;
