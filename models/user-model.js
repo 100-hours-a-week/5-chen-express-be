62ddb9a927a186ec78351955b70e2862dcd9907f
ZZ3n <ndsrhkd@naver.com>
2024-04-30 09:31:59 +0900

    add is_admin to user model

--- models/user-model.js
+++ models/user-model.js
@@ -6,14 +6,16 @@ module.exports = class {
     password = null;
     nickname = null;
     profile_image = null;
+    is_admin = false;
 
 
-    constructor(id, email, password, nickname, profile_image) {
+    constructor(id, email, password, nickname, profile_image, is_admin) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.nickname = nickname;
         this.profile_image = profile_image;
+        this.is_admin = is_admin;
     }
 
     static _loadJSON() {
@@ -22,7 +24,7 @@ module.exports = class {
 
     static create(email, password, nickname, profile_image) {
 
-        return new this(null, email, password, nickname, profile_image)
+        return new this(null, email, password, nickname, profile_image, false)
     }
 
     static find(id) {
@@ -32,7 +34,7 @@ module.exports = class {
 
         const target = _json_data.users[idx];
 
-        return new this(id, target.email, target.password, target.nickname, target.profile_image);
+        return new this(id, target.email, target.password, target.nickname, target.profile_image, target.is_admin);
     }
 
     static all() {
@@ -51,6 +53,7 @@ module.exports = class {
                     password: this.password,
                     nickname: this.nickname,
                     profile_image: this.profile_image,
+                    is_admin: this.is_admin,
                 }
             );
         } else {
@@ -60,6 +63,7 @@ module.exports = class {
             _json_data.users[idx].password = this.password;
             _json_data.users[idx].nickname = this.nickname;
             _json_data.users[idx].profile_image = this.profile_image;
+            _json_data.users[idx].is_admin = this.is_admin;
 
         }
         jsonWrite("users.json", _json_data)
@@ -70,6 +74,5 @@ module.exports = class {
         this.password = password;
         this.nickname = nickname;
         this.profile_image = filePath
-
     }
 }
