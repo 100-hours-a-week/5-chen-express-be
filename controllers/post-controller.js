dd2e617d346b71e2df60aab97cadb9a942eef92f
ZZ3n <ndsrhkd@naver.com>
2024-06-04 16:26:33 +0900

    fix json to mysql, post

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -2,128 +2,134 @@ const HttpStatus = require("http-status-codes");
 const {body, param} = require("express-validator");
 const PostModel = require("../models/post-model");
 const CommentModel = require("../models/comment-model");
+const {StatusCodes} = require("http-status-codes");
+const {post} = require("../models/comment-model");
 
 
 module.exports = new class {
-    comments = {
-        validator: [param("id").trim().isNumeric()],
-        controller: (req, res) => {
-            const post_id = req.params.id;
-            const sessionUser = req.session.user;
+  comments = {
+    validator: [param("id").trim().isNumeric()],
+    controller: (req, res) => {
+      const post_id = req.params.id;
+      const sessionUser = req.session.user;
 
-            const comments = CommentModel.findAllByPostId(post_id, sessionUser);
-            const post = PostModel.find(post_id);
+      const comments = CommentModel.findAllByPostId(post_id, sessionUser);
 
-            post.syncCommentCount(comments.length);
-            post.save();
-
-            res.json({
-                comments: comments,
-            });
-        }
+      res.json({
+        comments: comments,
+      });
     }
+  }
 
-    list = {
-        validator: [],
-        controller: (req, res) => {
-            res.json({
-                posts: PostModel.all()
-            });
-        }
+  list = {
+    validator: [],
+    controller: (req, res) => {
+      PostModel.all()
+        .then(posts => {
+          console.log(posts)
+          return res.json({
+            posts: posts
+          });
+        });
     }
+  }
 
-    detail = {
-        validator: [
-            param("id").trim().isNumeric(),
-        ],
-        controller: (req, res) => {
-            const id = req.params.id;
-            const post = PostModel.find(id);
-            const sessionUser = req.session.user;
+  detail = {
+    validator: [
+      param("id").trim().isNumeric(),
+    ],
+    controller: (req, res) => {
+      const id = req.params.id;
+      const sessionUser = req.session.user;
 
-            post.addViewCount();
-            post.save();
+      PostModel.find(id)
+        .then(post => {
+          post.addViewCount();
+          post.save();
 
-            res.json({
-                "post": post,
-                "can": post.can(sessionUser),
-            });
-        }
+          res.json({
+            "post": post,
+            "can": post.can(sessionUser),
+          });
+        });
     }
+  }
 
-    write = {
-        validator: [
-            body('title').trim().notEmpty(),
-            body('content').trim().notEmpty(),
-        ],
-        controller: (req, res) => {
-            const sessionUser = req.session.user;
-            const {title, content} = req.body;
-            const post = PostModel.create(title, content, req.file, sessionUser);
+  write = {
+    validator: [
+      body('title').trim().notEmpty(),
+      body('content').trim().notEmpty(),
+    ],
+    controller: (req, res) => {
+      const sessionUser = req.session.user;
+      const {title, content} = req.body;
+      const post = PostModel.create(title, content, req.file, sessionUser);
 
-            post.save();
+      post.save();
 
-            res.status(HttpStatus.CREATED);
+      res.status(HttpStatus.CREATED);
 
-            res.json({
-                "msg": "created",
-                post: post
-            });
-        }
+      res.json({
+        "msg": "created",
+        post: post
+      });
     }
+  }
 
-    delete = {
-        validator: [
-            param("id").trim().isNumeric()
-        ],
-        controller: (req, res) => {
-            const id = req.params.id;
-            const post = PostModel.find(id);
-            const sessionUser = req.session.user;
+  delete = {
+    validator: [
+      param("id").trim().isNumeric()
+    ],
+    controller: (req, res) => {
+      const id = req.params.id;
 
-            if (!post.can(sessionUser)) {
-                res.status(HttpStatus.FORBIDDEN);
-                res.json({
-                    msg: "FORBIDDEN",
-                    post: post
-                })
-                return;
-            }
-
+      const sessionUser = req.session.user;
+      PostModel.find(id)
+        .then(post => {
+          if (!post.can(sessionUser)) {
+            res.status(StatusCodes.FORBIDDEN);
+            res.json({
+              msg: "FORBIDDEN",
+              post: post
+            })
+          } else {
             post.delete();
 
             res.json({
-                msg: "deleted",
-                post: post
+              msg: "deleted",
+              post: post
             })
-        }
+          }
+        })
     }
+  }
 
-    update = {
-        validator: [
-            param("id").trim().isNumeric(),
-            body('title').trim().notEmpty(),
-            body('content').trim().notEmpty(),
-        ],
-        controller: (req, res) => {
-            const id = req.params.id;
-            const {title, content} = req.body;
-            const sessionUser = req.session.user;
+  update = {
+    validator: [
+      param("id").trim().isNumeric(),
+      body('title').trim().notEmpty(),
+      body('content').trim().notEmpty(),
+    ],
+    controller: (req, res) => {
+      const id = req.params.id;
+      const {title, content} = req.body;
+      const sessionUser = req.session.user;
 
-            const post = PostModel.find(id);
-            if (!post.can(sessionUser)) {
-                res.status(HttpStatus.FORBIDDEN);
-                res.json({
-                    msg: "FORBIDDEN",
-                    post: post
-                });
-                return;
-            }
-            post.update(title, content, req.file, sessionUser);
-            post.save();
+      PostModel.find(id)
+        .then(post => {
+          if (!post.can(sessionUser)) {
+            res.status(StatusCodes.FORBIDDEN);
+            res.json({
+              msg: "FORBIDDEN",
+              post: post
+            });
+            return;
+          }
+          post.update(title, content, req.file);
+          post.save();
 
-            res.json({"msg": "ok"});
-        }
-
+          res.json({"msg": "ok"});
+        });
     }
+  }
 }
--- models/post-model.js
+++ models/post-model.js
@@ -1,163 +1,177 @@
 const {findIndex, parseIntOrNull} = require("./utils");
+const {query} = require("./mysql-model");
 
 module.exports = class {
-    id = null;
-    title = null;
-    image = {
-        name: "default.jpg",
-        path: "http://localhost:8080/public/images/default.jpg"
-    };
-    content = null;
+  id = null;
+  title = null;
+  image = {
+    name: "default.jpg", path: "http://localhost:8080/public/images/default.jpg"
+  };
+  content = null;
 
-    author = {
-        id: null,
-        nickname: "DEFAULT",
-        profile_image: "http://localhost:8080/public/images/default.jpg"
-    };
+  author = {
+    id: null, nickname: "DEFAULT", profile_image: "http://localhost:8080/public/images/default.jpg"
+  };
 
-    created_at = null;
+  created_at = null;
 
-    like_count = 0;
-    comment_count = 0;
-    view_count = 0;
+  like_count = 0;
+  comment_count = 0;
+  view_count = 0;
 
-    constructor(id,
-                title, image, content,
-                author, created_at,
-                like_count = 0, comment_count = 0, view_count = 0) {
-        this.id = parseIntOrNull(id);
-        this.title = title;
-        this.image = image;
-        this.content = content;
+  constructor(id, title, image, content, author, created_at, like_count = 0, comment_count = 0, view_count = 0) {
+    this.id = parseIntOrNull(id);
+    this.title = title;
+    this.image = image;
+    this.content = content;
 
-        this.author.id = author ? parseInt(author.id) : null;
-        this.author.nickname = author ? author.nickname : "ERROR";
-        this.author.profile_image = author ? author.profile_image : "ERROR";
+    this.author.id = author ? parseInt(author.id) : null;
+    this.author.nickname = author ? author.nickname : "ERROR";
+    this.author.profile_image = author ? author.profile_image : "ERROR";
 
-        this.created_at = created_at;
+    this.created_at = created_at;
 
-        this.like_count = like_count;
-        this.comment_count = comment_count;
-        this.view_count = view_count;
+    this.like_count = like_count;
+    this.comment_count = comment_count;
+    this.view_count = view_count;
 
+  }
+
+  static of(row) {
+    return new this(
+      row.id,
+      row.title,
+      {name: "default.jpg", path: row.image,},
+      row.content,
+      {id: row.user_id, nickname: row.nickname, profile_image: row.profile_image},
+      row.created_at,
+      12_345,
+      row.comment_count,
+      row.view_count
+    );
+  }
+
+  static _loadJSON() {
+    return jsonParse("posts.json")
+  }
+
+  static create(title, content, file, author) {
+    let filename = "default.jpg";
+    let filePath = "http://localhost:8080/public/images/default.jpg";
+
+    if (file != null) {
+      filename = file.originalname;
+      filePath = `http://localhost:8080/uploads/${file.filename}`;
     }
 
-    static _loadJSON() {
-        return jsonParse("posts.json")
+    return new this(null, title, {name: filename, path: filePath}, content, {
+      id: author.id,
+      nickname: author.nickname,
+      profile_image: author.profile_image
+    }, new Date().toISOString(),)
+  }
+
+  static async all() {
+    return query(`
+        SELECT p.id,
+               p.title,
+               p.image,
+               p.content,
+               p.created_at,
+               p.view_count,
+               p.user_id,
+               u.nickname,
+               u.profile_image,
+               IFNULL(ccnt.cnt, 0) as comment_count
+        FROM Post as p
+                 JOIN User u on u.id = p.user_id
+                 LEFT JOIN (SELECT count(c.id) as cnt, c.post_id
+                            FROM Comment c
+                            GROUP BY c.post_id) as ccnt on ccnt.post_id = p.id
+        WHERE u.deleted_at IS NULL
+        ORDER BY p.created_at DESC
+    `,).then(rows => {
+      return rows.map(row => this.of(row))
+    })
+  }
+
+  static async find(id) {
+    return query(`
+        SELECT p.id,
+               p.title,
+               p.image,
+               p.content,
+               p.created_at,
+               p.view_count,
+               p.user_id,
+               u.nickname,
+               u.profile_image,
+               IFNULL(ccnt.cnt, 0) as comment_count
+        FROM Post as p
+                 JOIN User u on u.id = p.user_id
+                 LEFT JOIN (SELECT count(c.id) as cnt, c.post_id
+                            FROM Comment c
+                            GROUP BY c.post_id) as ccnt on ccnt.post_id = p.id
+        WHERE u.deleted_at IS NULL
+          AND p.id = ?
+        ORDER BY p.created_at DESC
+    `, id)
+      .then(rows => {
+        const row = rows[0]
+        return this.of(row);
+      });
+  }
+
+  save() {
+    if (this.id == null) {
+      query(
+        `INSERT INTO Post (title, image, user_id, content, created_at, view_count)
+         VALUES (?, ?, ?, ?, NOW(), ?)`,
+        this.title, this.image, this.author.id, this.content, this.view_count)
+        .then(rows => {
+          this.id = rows.insertId;
+        });
+    } else {
+      query(
+        `UPDATE Post
+         SET title      = ?,
+             image      = ?,
+             content    = ?,
+             view_count = ?,
+             updated_at = NOW()
+         WHERE id = ?;`, this.title, this.image.path, this.content, this.view_count, this.id)
+    }
+  }
+
+  update(title, content, file) {
+    let filename = this.image.name
+    let filePath = this.image.path
+
+    console.log(file);
+    if (file != null) {
+      filename = file.originalname;
+      filePath = `http://localhost:8080/uploads/${file.filename}`;
     }
 
-    static create(title, content, file, author) {
-        let filename = "default.jpg";
-        let filePath = "http://localhost:8080/public/images/default.jpg";
+    this.title = title;
+    this.content = content;
+    this.image = {name: filename, path: filePath};
+  }
 
-        if (file != null) {
-            filename = file.originalname;
-            filePath = `http://localhost:8080/uploads/${file.filename}`;
-        }
+  addViewCount() {
+    this.view_count = this.view_count + 1
+  }
 
-        return new this(
-            null, title, {name: filename, path: filePath}, content,
-            {id: author.id, nickname: author.nickname, profile_image: author.profile_image},
-            new Date().toISOString(),
-        )
-    }
-
-    static all() {
-        return this._loadJSON().posts.sort((a, b) => b.id - a.id);
-    }
-
-    static find(id) {
-        const _json_data = this._loadJSON()
-        const idx = findIndex(_json_data.posts, id)
-        const target = _json_data.posts[idx];
-
-        return new this(
-            id, target.title, target.image, target.content,
-            target.author, target.created_at,
-            target.like_count, target.comment_count, target.view_count
-        );
-    }
-
-    save() {
-        const _json_data = this.constructor._loadJSON()
-        if (this.id == null) {
-            const next_id = parseInt(_json_data.next_id);
-
-            _json_data.next_id = next_id + 1;
-            _json_data.posts.push(
-                {
-                    id: next_id,
-                    title: this.title,
-                    image: this.image,
-                    content: this.content,
-
-                    author: this.author,
-
-                    created_at: this.created_at,
-
-                    like_count: this.like_count,
-                    comment_count: this.comment_count,
-                    view_count: this.view_count,
-                }
-            );
-            this.id = next_id;
-        } else {
-            let idx = findIndex(_json_data.posts, this.id)
-
-            _json_data.posts[idx].title = this.title;
-            _json_data.posts[idx].image = this.image;
-            _json_data.posts[idx].content = this.content;
-
-            _json_data.posts[idx].author = this.author;
-
-            _json_data.posts[idx].created_at = this.created_at;
-
-            _json_data.posts[idx].like_count = this.like_count;
-            _json_data.posts[idx].comment_count = this.comment_count;
-            _json_data.posts[idx].view_count = this.view_count;
-
-        }
-
-        jsonWrite("posts.json", _json_data)
-    }
-
-    update(title, content, file, user) {
-        let filename = this.image.name
-        let filePath = this.image.path
-
-        console.log(file);
-        if (file != null) {
-            filename = file.originalname;
-            filePath = `http://localhost:8080/uploads/${file.filename}`;
-        }
-
-        this.title = title;
-        this.content = content;
-        this.image = {name: filename, path: filePath};
-        this.author = {id: user.id, nickname: user.nickname, profile_image: user.profile_image}
-    }
-
-    addViewCount() {
-        this.view_count = this.view_count + 1
-    }
-
-    syncCommentCount(count) {
-        this.comment_count = count;
-    }
-
-    can(user) {
-        if (user.is_admin) {
-            return true;
-        }
-        return parseInt(user.id) === this.author.id;
-    }
+  can(user) {
+    return parseInt(user.id) === this.author.id;
+  }
 
 
-    delete() {
-        const _json_data = this.constructor._loadJSON()
-        let idx = findIndex(_json_data.posts, this.id)
-        _json_data.posts.splice(idx, 1)
+  delete() {
+    const _json_data = this.constructor._loadJSON()
+    let idx = findIndex(_json_data.posts, this.id)
+    _json_data.posts.splice(idx, 1)
 
-        jsonWrite("posts.json", _json_data)
-    }
+    jsonWrite("posts.json", _json_data)
+  }
 }
